1bff85cce0881bdf115251bd596b6e128841b33a
Aditya Pandey <140270777+adityapandey78@users.noreply.github.com>
2024-11-30 16:15:58 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.svg'
+import tick from './tick.svg'
+import send from './send.png'
+import send2 from './send.svg'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    tick,
+    send,
+    send2
+}
